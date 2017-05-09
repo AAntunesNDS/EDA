69430ca81f0dd6b93a195f9d5a43de04d5a8a530
Arthur Antunes <arthur.antunes.aa@alu.ufc.br>
2017-05-08 21:15:26 -0300

    HashTable generico v2

--- EDAAlunos /src/br/ufc/quixada/eda/hash/Teste.java
+++ EDAAlunos /src/br/ufc/quixada/eda/hash/Teste.java
@@ -2,15 +2,15 @@ package br.ufc.quixada.eda.hash;
 
 public class Teste {
 	public static void main(String[] args) {
-		Hash hash = new EnderecamentoInterno(10);
-		//Hash hash = new EnderecamentoExterno(10);
+		Hash<String> hash = new EnderecamentoInterno(10);
+		//Hash<String> hash = new EnderecamentoExterno(10);
 		hash.inserir(1, "Afonso");
-		hash.inserir(10, "arthur");
+		hash.inserir(10, "ArthurAntunes");
 		hash.inserir(20, "Conflito");
 		hash.inserir(6, "Teste");
 		hash.inserir(12, "Neto");
-		hash.inserir(24, "fabio");
-		hash.inserir(18, "Tal");
+		hash.inserir(24, "Fulano");
+		hash.inserir(18, "Fabio");
 		System.out.println(hash.toString());
 		System.out.println(hash.remover(1));
 		System.out.println(hash.toString());
